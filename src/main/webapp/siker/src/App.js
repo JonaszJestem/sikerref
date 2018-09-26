c12428ed74fe5bd4eccf4de6a5cd500f57b43f1c
jwiacek <jonasz.wiacek@nokia.com>
2018-09-26 14:26:32 +0200

    added webpage template

--- src/main/webapp/siker/src/App.js
+++ src/main/webapp/siker/src/App.js
@@ -1,19 +1,82 @@
 import React, {Component} from 'react';
-import './App.css';
+import './styles.css';
 
 class App extends Component {
+
+
+    constructor(props) {
+        super(props);
+
+        this.handleSearchField = this.handleSearchField.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.renderOffers = this.renderOffers.bind(this);
+    }
+
+    state = {
+        afterFirstSearch: false,
+        searchQuery: "abc",
+    };
+
     render() {
+        this.handleSearch()
+        let collapse = this.state.afterFirstSearch ? 'collapse' : '';
         return (
-            <div className="App">
+            <div className={"App " + collapse}>
                 <form onSubmit={this.handleSearch}>
-                    <input type="search"/>
+                    <input onChange={this.handleSearchField} value={this.state.searchQuery}
+                           type="search"/>
                 </form>
+                <table>
+                    {this.renderOffers()}
+                </table>
             </div>
         );
     }
 
-    handleSearch() {
+    renderOffers() {
+        let contents = [];
+        if (this.state.afterFirstSearch) {
 
+
+            contents.push(<thead>
+            <tr>
+                <th>Image</th>
+                <th>Title</th>
+            </tr>
+            </thead>);
+            for (let i = 0; i < 3; i++) {
+                contents.push(<tr>
+                    <td>{this.state.offers[i].img}</td>
+                    <td>{this.state.offers[i].title}</td>
+                </tr>)
+            }
+
+        }
+        return contents;
+    }
+
+    handleSearchField(event) {
+        this.setState({searchQuery: event.target.value,});
+    }
+
+    async handleSearch(event) {
+        let offers = await(await(this.getOffers())).json();
+
+        this.setState({
+            afterFirstSearch: true,
+            searchQuery: "",
+            offers: offers
+        });
+    }
+
+
+    getOffers() {
+        try {
+            return fetch("http://localhost:3000/offers")
+        }
+        catch (e) {
+            return {};
+        }
     }
 }
 
